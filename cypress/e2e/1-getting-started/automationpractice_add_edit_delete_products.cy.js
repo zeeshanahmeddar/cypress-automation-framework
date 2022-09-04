c44a9c848ea4ef2482d9053baa1c0b6b58600d47
Zeeshan <071104005bbit@gmail.com>
2022-09-04 23:16:33 +0500

    updated the add, edit, delete test case

--- cypress/e2e/1-getting-started/automationpractice_add_edit_delete_products.cy.js
+++ cypress/e2e/1-getting-started/automationpractice_add_edit_delete_products.cy.js
@@ -17,13 +17,31 @@ describe('AutomationPractiveTestSuite',()=>{
         cy.get('a[title=Women]').should('be.visible').click()
         cy.get('.navigation_page').should('be.visible')
         cy.get('.button[data-id-product=1]').should('be.visible').click()
-        cy.get('.continue').should('be.visible').click()
+        cy.get('[title="Proceed to checkout"]').should('be.visible').click()
+        cy.get('a[title=Delete]').should('be.visible').click()
+        cy.get('.alert-warning').should('be.visible').click()
+        cy.get('a[title=Women]').should('be.visible').click()
+        
+        cy.get('.button[data-id-product=1]').should('be.visible').click()
+        cy.get('.continue[title="Continue shopping"]', { timeout: 30000 }).should('be.visible').click()
         cy.get('.button[data-id-product=2]').should('be.visible').click()
-        cy.get('.continue').should('be.visible').click()
+        cy.get('.continue[title="Continue shopping"]').should('be.visible').click()
         cy.get('.button[data-id-product=3]').should('be.visible').click()
-        cy.get('[title=Proceed to checkout]').should('be.visible').click()
+        cy.get('[title="Proceed to checkout"]').should('be.visible').click()
+
+        cy.get('td.cart_product > a > img[alt=Blouse]').should('be.visible').click()
+        cy.get('.button-plus').should('be.visible').click()
+        cy.get('#group_1').select('M')
+        cy.get('button.exclusive').should('be.visible').click()
+        cy.get('[title="Proceed to checkout"]').should('be.visible').click()
+       
         cy.get('.standard-checkout').should('be.visible').click()
-        cy.get('name=processAddress').should('be.visible').click()
+        cy.get('[name=processAddress]').should('be.visible').click()
+        cy.get('#uniform-cgv').should('be.visible').click()
+        cy.get('[name=processCarrier]').should('be.visible').click()
+        cy.get('.cheque').should('be.visible').click()
+        cy.get('.button-medium[type=submit]').should('be.visible').click()
+        cy.get('.alert-success').should('be.visible')
         
         
 
